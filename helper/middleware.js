38362e798d76c8d15b48a80a30f7b59accdfaca7
David Danh <danh.tu@jobsv.net>
2023-06-16 14:38:52 +0700

    update

--- helper/middleware.js
+++ helper/middleware.js
@@ -6,20 +6,22 @@ const { jwtHandle } = require('./handlePackage');
 const AuthMiddle = {
     isAuth: async (req, res, next) => {
     // Lấy access token từ header
-        const accessTokenFromHeader = req.headers.authorization;
-        if (!accessTokenFromHeader) {
+        // const accessTokenFromHeader = req.headers.authorization;
+        const accessToken = req.session.accessToken;
+        if (!accessToken) {
             return ResponseFail(res, "token not exits")
         }
         const accessTokenSecret = process.env.ACCESS_TOKEN_SECRET;
 
         const verified = await jwtHandle.verifyToken(
-            accessTokenFromHeader,
+            accessToken,
             accessTokenSecret,
         );
         if (!verified) return ResponseFail(res, "you can not connect link!")
 
         const users = await AuthModel.getUser(verified.payload.username);
         req.user = users[0];
+        console.log(users)
         return next();
     }
 }
